3a02ccd9aa2ecf853ac723bad5b3c0b9f2f269bf
diegussdev <diegussdev@gmail.com>
2022-06-30 20:38:41 -0300

    Adiciona model Api

--- endpoint_manager/admin.py
+++ endpoint_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from endpoint_manager.models import Api
+
 # Register your models here.
+
+admin.site.register(Api)
--- endpoint_manager/models.py
+++ endpoint_manager/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Api(models.Model):
+    base_route = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
