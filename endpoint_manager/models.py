d1fa02c21f8b712e57dac6a41e503ecd3e1d1b8e
Yuri Martins <yuri.olivera@hotmail.com>
2022-06-30 23:32:02 -0300

    gitignore

--- endpoint_manager/models.py
+++ endpoint_manager/models.py
@@ -34,7 +34,7 @@ class Endpoint(models.Model):
 		('XML', RetornosRequisicao.XML.value)
 	)
 
-	endpoint = models.CharField(max_length=120, default='', unique=True)
+	endpoint = models.CharField(max_length=90, default='', unique=True)
 	status_code = models.IntegerField(default=200)
 	metodo_http = models.CharField(max_length=10, choices=METODOS_REQUISICAO, default=MetodosRequisicao.GET.value)
 	tipo_retorno = models.CharField(max_length=10, choices=TIPO_RETORNO, default=RetornosRequisicao.JSON.value)
