d9b275ee7db260dfe5485a857fa169a6412bde1c
Yuri Martins <yuri.olivera@hotmail.com>
2022-06-30 21:13:39 -0300

    Adição de model Endpoint

--- endpoint_manager/admin.py
+++ endpoint_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from endpoint_manager.models import Api
+from endpoint_manager.models import *
 
 # Register your models here.
 
 admin.site.register(Api)
+admin.site.register(Endpoint)
--- endpoint_manager/models.py
+++ endpoint_manager/models.py
@@ -1,9 +1,39 @@
 from pyexpat import model
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+
+class MetodosRequisicao(Enum):
+	GET = 'GET'
+	POST = 'POST'
+	PUT = 'PUT'
+	DELETE = 'DELETE'
+	PATCH = 'PATCH'
+
+class RetornosRequisicao(Enum):
+	JSON = 'JSON'
+	XML = 'XML'
 
 class Api(models.Model):
     base_route = models.CharField(max_length=50)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Endpoint(models.Model):
+	METODOS_REQUISICAO = (
+        ('GET', MetodosRequisicao.GET.value),
+        ('POST', MetodosRequisicao.POST.value),
+        ('PUT', MetodosRequisicao.PUT.value),
+        ('DELETE', MetodosRequisicao.DELETE.value),
+        ('PATCH', MetodosRequisicao.PATCH)
+    )
+
+	TIPO_RETORNO = (
+		('JSON', RetornosRequisicao.JSON.value),
+		('XML', RetornosRequisicao.XML.value)
+	)
+
+	metodo_http = models.CharField(max_length=10, choices=METODOS_REQUISICAO, default=MetodosRequisicao.GET.value)
+	tipo_retorno = models.CharField(max_length=10, choices=TIPO_RETORNO, default=RetornosRequisicao.JSON.value)
+	retorno = models.TextField()
