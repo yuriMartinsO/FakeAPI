d16a397717b98973d758319c477f427ffc5307ac
yurinhomartins <yuri.olivera@hotmail.com>
2022-07-07 00:58:14 -0300

    mudança de url

--- FakeAPI/settings.py
+++ FakeAPI/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 
 # Base Url
-SITE_URL = 'http://localhost:8000'
+SITE_URL = 'https://fake-api-dy.herokuapp.com'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
